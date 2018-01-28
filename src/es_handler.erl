2b8af1a69613fd39cd37253714b04ba041d484b6
Valerii Tikhonov <com.tihon@mail.ru>
2018-01-28 11:46:29 +0100

    add release and magic greeter

--- src/es_handler.erl
+++ src/es_handler.erl
@@ -3,12 +3,13 @@
 -export([init/2]).
  
 -define(JSON, #{<<"content-type">> => <<"application/json">>}).
--define(MSG, <<"hello world">>). 
 
 init(Req0, State) ->
   {ok, Vsn} = application:get_key(example_service, vsn),
   Path = cowboy_req:path(Req0),
   Qs = cowboy_req:qs(Req0),
   es_log:info("New request ~p : ~p", [Path, Qs]),
-  Req = cowboy_req:reply(200, ?JSON, jsone:encode(#{<<"vsn">> => list_to_binary(Vsn), <<"msg">> => ?MSG}), Req0),
+  {ok, Greeting} = application:get_env(example_service, greeting),
+  Req = cowboy_req:reply(200, ?JSON,
+    jsone:encode(#{<<"vsn">> => list_to_binary(Vsn), <<"msg">> => list_to_binary(Greeting)}), Req0),
   {ok, Req, State}.
